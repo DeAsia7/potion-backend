cc8d36932cfcf5df3737a059f322b7d78b6cc02f
DeAsia7 <asiamallory277@gmail.com>
2025-06-14 13:17:45 -0400

    add

--- routes/auth.js
+++ routes/auth.js
@@ -6,10 +6,22 @@ import {eq, and} from 'drizzle-orm';
 const router = express.Router();
 
 //use routers to connect. 
-
+//this is how to login into database on postman
 router.post('/login', async (req, res) => {
 const authHeader = req.headers['authorization'];
 if (!authHeader || !authHeader.startsWith("Basic")){
     return res.status(401).json({message: 'Missing Basic Authentication Header'});
 }
+const base64Credentials = authHeader.split(' ')[1];
+const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+const [username, password] = credentials.split(':');
+
+const user = db.select().from(users).where(and(eq(username, users.username), eq(password, users.password)))
+if (!user) {
+    return res.status(401).json({message: 'Invalid credentials'});
+}
+
+const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '8h' });
+res.json({ token });
 })
+export default router;
